b97195df715458bc5f2fdf9da7c7c53a3610d987
vli21 <vli21@stuy.edu>
2020-11-23 20:43:25 -0500

    method add works

--- RealNumber.java
+++ RealNumber.java
@@ -38,7 +38,8 @@ public class RealNumber{
  public RealNumber add(RealNumber other){
     //other can be ANY RealNumber, including a RationalNumber
     //or other subclasses of RealNumber (that aren't written yet)
-    return null;
+    RealNumber sum= new RealNumber(this.value+other.value);
+    return sum;
  }
 
  /*
